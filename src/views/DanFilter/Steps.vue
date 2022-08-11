518f875e9cfac5adfd8e764cdf2335f7a70d7e9f
xrz-cloud <3545270592@qq.com>
2022-08-11 12:38:57 +0800

    新增 弹幕过滤

---
+++ src/views/DanFilter/Steps.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from "vue";
+defineProps<{
+  step: Number;
+}>();
+</script>
+<template>
+  <el-steps :active="step" finish-status="success">
+    <el-step title="过滤规则" description="请上传弹幕屏蔽规则文件" />
+    <el-step title="弹幕文件" description="请上传弹幕文件" />
+    <el-step title="过滤完成" description="选择生成细节" />
+  </el-steps>
+</template>
+<style></style>
